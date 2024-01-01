63098657b9d7f12d9278fe0bb625a73b1de25b94
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-01 21:05:09 +0545

    route

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,50 +2,44 @@ import React from "react";
 
 export default function About() {
   return (
-      <div className="container w-full px-4 bg-gray-500">
+      <div className="container w-full py-5 px-4 bg-gray-500">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="bg-gray-50 px-8 py-10 rounded-md">
-            <div className="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
-              <i data-feather="activity"></i>
-            </div>
-            <h4 className="font-medium text-gray-700 text-lg mb-4">
-              High experience
-            </h4>
-            <p className="font-normal text-gray-500 text-md">
-              Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit,
-              sed do eiusmod tempor <br /> incididunt ut labore et dolore magna
-              aliqua.
-            </p>
-          </div>
+  <div className="bg-blue-400 px-8 py-10 rounded-md transition duration-300 ease-in-out hover:bg-blue-500">
+    <div className="w-20 py-3 flex justify-center bg-gray-100 rounded-md mb-4">
+    <i class="ri-bar-chart-box-fill text-3xl"></i>
+    </div>
+    <h4 className="font-medium text-gray-700 text-lg mb-4">
+      My Experience
+    </h4>
+    <p className="font-normal text-white text-md">
+      With years of dedicated work across various industries and technologies, I bring a wealth of experience to the table. Tackling diverse challenges has honed my skills, and I'm ready to apply this expertise to your projects.
+    </p>
+  </div>
 
-          <div className="bg-gray-50 px-8 py-10 rounded-md">
-            <div className="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
-              <i data-feather="codesandbox"></i>
-            </div>
-            <h4 className="font-medium text-gray-700 text-lg mb-4">
-              Useful sandboxes
-            </h4>
-            <p className="font-normal text-gray-500 text-md">
-              Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit,
-              sed do eiusmod tempor <br /> incididunt ut labore et dolore magna
-              aliqua.
-            </p>
-          </div>
+  <div className="bg-blue-400 px-8 py-10 rounded-md transition duration-300 ease-in-out hover:bg-blue-500">
+    <div className="w-20 py-3 flex justify-center bg-gray-100 rounded-md mb-4">
+    <i class="ri-edit-box-fill text-3xl"></i>
+    </div>
+    <h4 className="font-medium text-gray-700 text-lg mb-4">
+      Practical Sandboxes
+    </h4>
+    <p className="font-normal text-gray-500 text-md">
+      My collection of practical sandboxes serves as invaluable resources for development and testing. These environments facilitate experimentation and enable me to create robust, scalable solutions tailored to your needs.
+    </p>
+  </div>
 
-          <div className="bg-gray-50 px-8 py-10 rounded-md">
-            <div className="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
-              <i data-feather="coffee"></i>
-            </div>
-            <h4 className="font-medium text-gray-700 text-lg mb-4">
-              Success side projects
-            </h4>
-            <p className="font-normal text-gray-500 text-md">
-              Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit,
-              sed do eiusmod tempor <br /> incididunt ut labore et dolore magna
-              aliqua.
-            </p>
-          </div>
-        </div>
-      </div>
+  <div className="bg-blue-400 px-8 py-10 rounded-md transition duration-300 ease-in-out hover:bg-blue-500">
+    <div className="w-20 py-3 flex justify-center bg-gray-100 rounded-md mb-4">
+    <i class="ri-terminal-box-fill text-3xl"></i>
+    </div>
+    <h4 className="font-medium text-gray-700 text-lg mb-4">
+      Successful Side Projects
+    </h4>
+    <p className="font-normal text-gray-500 text-md">
+      Beyond my core work, I've embarked on and completed numerous side projects that have flourished into successful endeavors. These ventures showcase my innovative spirit, problem-solving abilities, and determination to deliver outstanding results.
+    </p>
+  </div>
+</div>
+</div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,15 @@
 import React from "react";
 import rudal from "../assets/images/rudalking.png";
-import backimg from "../assets/images/download.jpg";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-full justify-between bg-gradient-to-r from-slate-900 to-slate-700 text-white">
+    <div className="flex flex-col h-screen justify-between bg-gradient-to-r from-slate-900 to-slate-700 text-white">
+      <div className=" flex flex-col justify-center">
       <nav className="p-4 text-right">
         <button className="px-4 md:px-7 py-2 bg-gray-300 font-medium md:font-semibold text-gray-700 text-sm md:text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500">
           Get my CV
         </button>
       </nav>
-
-      <div className=" flex flex-col justify-center">
         <div className="flex justify-center ">
           <img
             src={rudal}
@@ -19,7 +17,6 @@ export default function Home() {
             className="filter grayscale h-64 w-64 rounded-full shadow-lg"
           />
         </div>
-
         <h6 className="text-center pt-4 font-medium text-gray-300 text-lg md:text-2xl uppercase mb-2">
           Rudal Kunwar
         </h6>
